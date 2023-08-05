e115d8bf5e5541d8a0374f3cc4b66752f69dcd5a
Yash Pratap Singh <yashsingh20001@gmail.com>
2023-08-05 07:47:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, request, redirect
+import pickle
+from generate_caption import gen
+import warnings
+warnings.filterwarnings("ignore")
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/', methods = ['POST'])
+def upload_file():
+	if request.method == 'POST':
+		img = request.files['image']
+
+		# print(img)
+		# print(img.filename)
+
+		img.save("static/"+img.filename)
+
+	
+		caption = gen("static/"+img.filename)
+
+		
+		result_dic = {
+			'image' : "static/" + img.filename,
+			'description' : caption
+		}
+	return render_template('index.html', results = result_dic)
+
+if __name__ == '__main__':
+	app.run(debug = True)
